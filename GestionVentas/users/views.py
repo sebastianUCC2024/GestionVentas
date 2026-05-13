79f0f0752296085d4eb5667d0101ee865db922a0
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-05-13 09:07:49 -0500

    Se creo al urls de la app usuarios y se corrigo view

--- GestionVentas/users/views.py
+++ GestionVentas/users/views.py
@@ -1,25 +1,40 @@
-from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import TemplateView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView as DjangoLoginView
+from django.contrib.auth.views import LogoutView as DjangoLogoutView
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from .models import User
 from .forms import ClienteRegistroForm, VendedorRegistroForm
 
-# Create your views here.
-class LoginView(LoginView):
+
+class UserLoginView(DjangoLoginView):
     template_name = 'users/login.html'
 
-class LogoutView(LogoutView):
+    def get_success_url(self):
+        user = self.request.user
+
+        if user.rol == User.Roles.CLIENTE:
+            return reverse_lazy('cliente_dashboard')
+
+        elif user.rol == User.Roles.VENDEDOR:
+            return reverse_lazy('vendedor_dashboard')
+
+        elif user.rol == User.Roles.ADMIN:
+            return reverse_lazy('admin_dashboard')
+
+        return reverse_lazy('login')
+
+
+class UserLogoutView(DjangoLogoutView):
     template_name = 'users/logout.html'
+
 
 class ClienteRegisterView(CreateView):
     form_class = ClienteRegistroForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
 
+
 class VendedorRegisterView(CreateView):
     form_class = VendedorRegistroForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
-
-
-
