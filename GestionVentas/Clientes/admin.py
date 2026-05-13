caedd3a7c8512cd891ebc1ab7dca963baa60ab57
Gabonarvaezm <160960360+Gabonarvaezm@users.noreply.github.com>
2026-05-13 07:31:08 -0500

    subiendo admin

--- GestionVentas/Clientes/admin.py
+++ GestionVentas/Clientes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cliente
 
-# Register your models here.
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'documento', 'correo', 'telefono', 'estado', 'vendedor', 'fecha_registro')
+    search_fields = ('nombre', 'documento', 'correo')
+    list_filter = ('estado', 'fecha_registro')
