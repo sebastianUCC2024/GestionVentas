531e4d5122faf3c1259d212f94e5c5ace83d27bc
Andresittow <edison.chacuaq@campusucc.edu.co>
2026-05-13 09:15:05 -0500

    Subiendo cambios

--- GestionVentas/GestionVentas/urls.py
+++ GestionVentas/GestionVentas/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
+    path('ventas/', include('ventas.urls')),
 ]
