bf62cb66afdb580e7bf657c140a76a83f7225bd7
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-05-20 09:03:12 -0500

    Se corregio el views

--- GestionVentas/users/views.py
+++ GestionVentas/users/views.py
@@ -2,39 +2,64 @@ from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.contrib.auth.views import LogoutView as DjangoLogoutView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.contrib import messages
+
 from .models import User
 from .forms import ClienteRegistroForm, VendedorRegistroForm
 
 
 class UserLoginView(DjangoLoginView):
+
     template_name = 'users/login.html'
 
     def get_success_url(self):
+
         user = self.request.user
 
         if user.rol == User.Roles.CLIENTE:
-            return reverse_lazy('cliente_dashboard')
+            return reverse_lazy('cliente_list')
 
         elif user.rol == User.Roles.VENDEDOR:
-            return reverse_lazy('vendedor_dashboard')
+            return reverse_lazy('venta_list')
 
         elif user.rol == User.Roles.ADMIN:
-            return reverse_lazy('admin_dashboard')
+            return reverse_lazy('ventas_dashboard')
 
-        return reverse_lazy('login')
+        return reverse_lazy('cliente_list')
 
 
 class UserLogoutView(DjangoLogoutView):
+
     template_name = 'users/logout.html'
 
 
 class ClienteRegisterView(CreateView):
+
     form_class = ClienteRegistroForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
 
+    def form_valid(self, form):
+
+        messages.success(
+            self.request,
+            'Cuenta de cliente creada correctamente.'
+        )
+
+        return super().form_valid(form)
+
 
 class VendedorRegisterView(CreateView):
+
     form_class = VendedorRegistroForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+
+        messages.success(
+            self.request,
+            'Cuenta de vendedor creada correctamente.'
+        )
+
+        return super().form_valid(form)
