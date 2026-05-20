3184a509576620c0c1590da96da7d4f2c0526dc8
TatianaUCC <jennifer.torresgo@campusucc.edu.co>
2026-05-20 09:49:19 -0500

    feat(ventas): actualizar rutas del módulo de ventas

--- GestionVentas/ventas/urls.py
+++ GestionVentas/ventas/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.venta_list, name='venta_list'),
+    path('dashboard/', views.ventas_dashboard, name='ventas_dashboard'),
+
     path('crear/', views.venta_create, name='venta_create'),
     path('<int:pk>/', views.venta_detail, name='venta_detail'),
     path('<int:pk>/editar/', views.venta_update, name='venta_update'),
@@ -13,6 +15,4 @@ urlpatterns = [
     path('seguimientos/<int:pk>/', views.seguimiento_detail, name='seguimiento_detail'),
     path('seguimientos/<int:pk>/editar/', views.seguimiento_update, name='seguimiento_update'),
     path('seguimientos/<int:pk>/eliminar/', views.seguimiento_delete, name='seguimiento_delete'),
-
-    path('dashboard/', views.ventas_dashboard, name='ventas_dashboard'),
 ]
