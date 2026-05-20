3d4ec5f79129dd24e780fcbfca3d330140b2366b
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-05-20 08:41:14 -0500

    Se corrigio las urls

--- GestionVentas/users/urls.py
+++ GestionVentas/users/urls.py
@@ -1,23 +1,9 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from .views import UserLoginView, UserLogoutView, ClienteRegisterView, VendedorRegisterView
 
 urlpatterns = [
-    path(
-        'login/',
-        auth_views.LoginView.as_view(template_name='users/login.html'),
-        name='login'
-    ),
-
-    path(
-        'logout/',
-        auth_views.LogoutView.as_view(template_name='users/logout.html'),
-        name='logout'
-    ),
-
-    path(
-        'registro/',
-        views.register_view,
-        name='register'
-    ),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('registro/cliente/', ClienteRegisterView.as_view(), name='cliente_register'),
+    path('registro/vendedor/', VendedorRegisterView.as_view(), name='vendedor_register'),
 ]
