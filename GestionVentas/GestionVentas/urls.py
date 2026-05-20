619dcfa9e0f6fe4cabc9c14e17f36c146cf2f76d
Gabonarvaezm <160960360+Gabonarvaezm@users.noreply.github.com>
2026-05-20 08:31:04 -0500

    subimos html base

--- GestionVentas/GestionVentas/urls.py
+++ GestionVentas/GestionVentas/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/clientes/')),
+
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
     path('ventas/', include('ventas.urls')),
