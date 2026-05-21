dc472b624e3b91a9a577e29b693c6823970f2d20
sebastianUCC2024 <juans.patino@campusucc.edu.co>
2026-05-20 21:17:11 -0500

    Se corregio errores de los templates

--- GestionVentas/users/views.py
+++ GestionVentas/users/views.py
@@ -9,57 +9,42 @@ from .forms import ClienteRegistroForm, VendedorRegistroForm
 
 
 class UserLoginView(DjangoLoginView):
-
     template_name = 'users/login.html'
 
     def get_success_url(self):
-
         user = self.request.user
 
         if user.rol == User.Roles.CLIENTE:
             return reverse_lazy('cliente_list')
 
-        elif user.rol == User.Roles.VENDEDOR:
+        if user.rol == User.Roles.VENDEDOR:
             return reverse_lazy('venta_list')
 
-        elif user.rol == User.Roles.ADMIN:
+        if user.rol == User.Roles.ADMIN:
             return reverse_lazy('ventas_dashboard')
 
         return reverse_lazy('cliente_list')
 
 
 class UserLogoutView(DjangoLogoutView):
-
     next_page = reverse_lazy('login')
 
 
 class ClienteRegisterView(CreateView):
-
     form_class = ClienteRegistroForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
 
     def form_valid(self, form):
-
-        messages.success(
-            self.request,
-            'Cuenta de cliente creada correctamente.'
-        )
-
+        messages.success(self.request, 'Cuenta de cliente creada correctamente.')
         return super().form_valid(form)
 
 
 class VendedorRegisterView(CreateView):
-
     form_class = VendedorRegistroForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('login')
 
     def form_valid(self, form):
-
-        messages.success(
-            self.request,
-            'Cuenta de vendedor creada correctamente.'
-        )
-
+        messages.success(self.request, 'Cuenta de vendedor creada correctamente.')
         return super().form_valid(form)
